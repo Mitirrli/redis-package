4e33b3ab63535b6ba7c30a2dec2712226e889351
huanglei <huanglei@quanjingshuju.com>
2020-08-10 18:33:55 +0800

    Add lock trait

--- src/Traits/LockTrait.php
+++ src/Traits/LockTrait.php
@@ -2,4 +2,58 @@
 
 declare(strict_types=1);
 
-namespace Mitirrli\RedisPackage\
+namespace Mitirrli\RedisPackage\Traits;
+
+use Mitirrli\RedisPackage\Exception\KeyException;
+use Mitirrli\RedisPackage\Constant\Lock as LockConstant;
+
+trait LockTrait
+{
+    /**
+     * Lock key.
+     */
+    protected $key;
+
+    /**
+     * Lock value.
+     */
+    protected $val;
+
+    /**
+     * Lock time.
+     */
+    protected $time = LockConstant::DEFAULT_LOCK_TIME;
+
+    /**
+     * Set key.
+     *
+     * @param string $key
+     * @throws KeyException
+     */
+    public function setKey(string $key)
+    {
+        if (empty($key)) {
+            throw new KeyException('Key can not be empty string.', 2);
+        }
+
+        $this->key = sprintf(LockConstant::LOCK_NAME, $key);
+    }
+
+    /**
+     * Set random value.
+     */
+    public function setValue()
+    {
+        $this->val = microtime(true) . uniqid() . ip2long($_SERVER['REMOTE_ADDR']) . mt_rand(1, 9999);
+    }
+
+    /**
+     * Set lock time.
+     *
+     * @param $time
+     */
+    public function setTime(int $time)
+    {
+        $this->time = $time;
+    }
+}
